7f0e69e28e46b36d37c17f31bdf797602dc2bab0
Eric Chrobak <eric@ericchrobak.com>
2018-01-12 17:16:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+var kittens = function(array) {
+  return array;
+}
+
+var destructivelyAppendKitten = function(name) {
+  kittens.push(name)
+  return kittens
+}
+
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+var destructivelyRemoveLastKitten = function() {
+  kittens.pop()
+  return kittens
+}
+
+var destructivelyRemoveFirstKitten = function() {
+  kittens.shift()
+  return kittens
+}
+
+var appendKitten = function(name) {
+  var kit = [kittens, ...name]
+  return kit
+}
